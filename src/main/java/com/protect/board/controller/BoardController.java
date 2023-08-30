0f84a8f4e152ccbf7eb7040e5a950a7e28560158
박규민 <131234408+kyumin7487@users.noreply.github.com>
2023-08-31 02:02:36 +0900

    서버 개발

---
+++ src/main/java/com/protect/board/controller/BoardController.java
@@ -0,0 +1,14 @@
+package com.protect.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BoardController {
+
+    @GetMapping("/board/write") //localhost:8080/board/write
+    public String boardwriteForm() {
+        return "boardwrite";
+    }
+}
